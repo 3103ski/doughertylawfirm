3d56db21215887f429704716889626238e4aaaf8
3103ski <bjastski@gmail.com>
2020-12-05 21:37:57 -0800

    troubleshooting forms

--- src/components/formComponents/contactForm/ContactFormComponent.js
+++ src/components/formComponents/contactForm/ContactFormComponent.js
@@ -37,7 +37,8 @@ class ContactForm extends Component {
 					<input type='email' name='email' />
 					<textarea name='feedback'></textarea>
 				</form>
-				<Form style={{ width: '100%' }} model='contactForm' onSubmit={(values) => this.handleSubmit(values)} method='post' data-netlify='true' name='contact-dougherty'>
+				<Form style={{ width: '100%' }} model='contactForm' onSubmit={(values) => this.handleSubmit(values)} method='post' data-netlify='true' netlify name='contact-dougherty'>
+					{/* <Form style={{ width: '100%' }} model='contactForm' onSubmit={(values) => this.handleSubmit(values)} method='post' data-netlify='true' netlify name='contact-dougherty'> */}
 					<Row className='form-group'>
 						<Col className='col-12 col-lg-6'>
 							<Label htmlFor='firstName' className='d-none'>
