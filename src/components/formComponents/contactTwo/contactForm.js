d7bd2aecaa3e009ac3cca382a9ed295d89699487
3103ski <bjastski@gmail.com>
2020-12-05 22:17:10 -0800

    yet another netlify form test

--- src/components/formComponents/contactTwo/contactForm.js
+++ src/components/formComponents/contactTwo/contactForm.js
@@ -8,27 +8,29 @@ const ContactForm = (props) => {
 				<input type='email' name='email' />
 				<textarea name='message'></textarea>
 			</form>
-			<form method='post' data-netlify='true' name='contact-dougherty'>
-				<div className='fields'>
-					<div className='field'>
-						<input type='hidden' name='form-name' value='contact-dougherty' />
-					</div>
-					<div className='field'>
-						<label htmlFor='name'>Name</label>
-						<input type='text' name='name' id='name' />
-					</div>
-					<div className='field'>
-						<label htmlFor='email'>Email</label>
-						<input type='email' name='email' id='email' />
-					</div>
-					<div className='field'>
-						<label htmlFor='message'>Message</label>
-						<textarea name='message' id='message' rows='4'></textarea>
-					</div>
+
+			<form name='contact-carirae' method='post' data-netlify='true'>
+				<input type='hidden' name='form-name' value='contact-carirae' />
+				<div className='field half first'>
+					<label htmlFor='name'>Name</label>
+					<input type='text' name='name' id='name' />
+				</div>
+				<div className='field half'>
+					<label htmlFor='email'>Email</label>
+					<input type='text' name='email' id='email' />
+				</div>
+				<div className='field'>
+					<label htmlFor='message'>Message</label>
+					<textarea name='message' id='message' rows='4'></textarea>
 				</div>
 				<ul className='actions'>
 					<li>
-						<input type='submit' value='Send Message' />
+						<button type='submit' className='special'>
+							SUBMIT
+						</button>
+					</li>
+					<li>
+						<input name='reset-form' type='reset' value='Reset' />
 					</li>
 				</ul>
 			</form>
