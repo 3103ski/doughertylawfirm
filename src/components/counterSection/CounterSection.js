6b7f68bab210325dc424235428cbeeb650929065
3103ski <bjastski@gmail.com>
2020-12-18 13:46:30 -0800

    client revisions to hover boxes and counters

--- src/components/counterSection/CounterSection.js
+++ src/components/counterSection/CounterSection.js
@@ -8,11 +8,27 @@ export default function CounterSection(props) {
 		<LayoutSection
 			filterColor='rgba(55,63,69,0.6)'
 			fullWidth
-			containerStyle={{ backgroundImage: 'url(/assets/images/homePage/justice-bg-1.jpg)', position: 'relative' }}
-			rowStyle={{ maxWidth: 'var(--container-width)', display: 'flex', justifyContent: 'center', margin: 'auto', padding: '60px 0' }}>
+			containerStyle={{
+				backgroundImage:
+					'url(/assets/images/homePage/justice-bg-1.jpg)',
+				position: 'relative',
+			}}
+			rowStyle={{
+				maxWidth: 'var(--container-width)',
+				display: 'flex',
+				justifyContent: 'center',
+				margin: 'auto',
+				padding: '60px 0',
+			}}>
 			<Col className='counter-col col-6 col-md-4 col-lg-2'>
 				<Counter.Card title='scooter' line2='crash'>
-					<Counter isCurrency num='2.5' after='m' speed={200} incr={1} />
+					<Counter
+						isCurrency
+						num='2.5'
+						after='m'
+						speed={200}
+						incr={0.5}
+					/>
 				</Counter.Card>
 			</Col>
 			<Col className='counter-col col-6 col-md-4 col-lg-2'>
@@ -22,12 +38,24 @@ export default function CounterSection(props) {
 			</Col>
 			<Col className='counter-col col-6 col-md-4 col-lg-2'>
 				<Counter.Card title='car' line2='crash'>
-					<Counter isCurrency num='600' after='k' speed={9} incr={5} />
+					<Counter
+						isCurrency
+						num='600'
+						after='k'
+						speed={9}
+						incr={5}
+					/>
 				</Counter.Card>
 			</Col>
 			<Col className='counter-col col-6 col-md-4 col-lg-2'>
 				<Counter.Card title='car' line2='crash'>
-					<Counter isCurrency num='525' after='k' speed={10} incr={5} />
+					<Counter
+						isCurrency
+						num='525'
+						after='k'
+						speed={10}
+						incr={5}
+					/>
 				</Counter.Card>
 			</Col>
 			<Col className='counter-col col-6 col-md-4 col-lg-2'>
--- src/components/hoverServices/HoverServices.js
+++ src/components/hoverServices/HoverServices.js
@@ -9,22 +9,52 @@ export default function HoverServices(props) {
 		<>
 			<LayoutSection rowStyle={{ padding: '40px 0' }}>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/concussion.jpg`} icon='fas fa-brain' title='concussions' link={ROUTES.CONCUSSION} />
+					<HoverBox
+						image={`${hovAssets}/concussion.jpg`}
+						icon='fas fa-brain'
+						title='brain injury'
+						link={ROUTES.CONCUSSION}
+					/>
 				</Col>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/mvc.jpg`} icon='fas fa-car-crash' title='mvc' link={ROUTES.MOTOR_VEHICLE_CRASH} />
+					<HoverBox
+						image={`${hovAssets}/mvc.jpg`}
+						icon='fas fa-car-crash'
+						title='motor vehicle crash'
+						link={ROUTES.MOTOR_VEHICLE_CRASH}
+					/>
 				</Col>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/partner.jpg`} icon='fas fa-truck' title='truck crash' link={ROUTES.TRUCKING} />
+					<HoverBox
+						image={`${hovAssets}/truck.jpg`}
+						icon='fas fa-truck'
+						title='truck crash'
+						link={ROUTES.TRUCKING}
+					/>
 				</Col>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/truck.jpg`} icon='fas fa-exclamation-triangle' title='wrongful death' link={ROUTES.WRONGFUL_DEATH} />
+					<HoverBox
+						image={`${hovAssets}/wrongful.jpg`}
+						icon='fas fa-exclamation-triangle'
+						title='wrongful death'
+						link={ROUTES.WRONGFUL_DEATH}
+					/>
 				</Col>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/unconscious.jpg`} icon='fas fa-question circle' title='loss of consciousness' link={ROUTES.LOSS_OF_CONSCIOUSNESS} />
+					<HoverBox
+						image={`${hovAssets}/wet-floor-1.jpg`}
+						icon='fas fa-building'
+						title='Premise Liability'
+						link={ROUTES.PREMISE_LIABILITY}
+					/>
 				</Col>
 				<Col className='col-6 col-lg-4'>
-					<HoverBox image={`${hovAssets}/wrongful.jpg`} icon='fas fa-handshake' title='outside attorney consults' link={ROUTES.OUTSIDE_ATTORNEY_CONSULTS} />
+					<HoverBox
+						image={`${hovAssets}/partner.jpg`}
+						icon='fas fa-handshake'
+						title='outside attorney consults'
+						link={ROUTES.OUTSIDE_ATTORNEY_CONSULTS}
+					/>
 				</Col>
 			</LayoutSection>
 		</>
